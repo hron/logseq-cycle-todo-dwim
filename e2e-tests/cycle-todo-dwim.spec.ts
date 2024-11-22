3b4fdaedf83b7bac22a0c6560b40a78f953a1aa7
Aleksei Gusev <aleksei.gusev@gmail.com>
2024-11-22 17:21:37 +0200

    Implement `cycle-todo-dwim` for non-scheduled todo

--- e2e-tests/cycle-todo-dwim.spec.ts
+++ e2e-tests/cycle-todo-dwim.spec.ts
@@ -6,22 +6,37 @@ import { callPageAPI, createRandomPage, modKey } from './utils'
 test('cycling TODO state', async ({ page, block }) => {
   await createRandomPage(page)
 
-  await block.mustFill('foobar')
+  await block.mustFill('foo')
+  await block.enterNext()
+  await block.mustFill('bar')
   await block.escapeEditing()
+  await page.keyboard.press('ArrowUp', { delay: 10 })
+  await page.keyboard.press('ArrowUp', { delay: 10 })
+  await page.keyboard.down('Shift')
   await page.keyboard.press('ArrowDown', { delay: 10 })
+  await page.keyboard.up('Shift')
+  await block.waitForSelectedBlocks(2)
+
   for (const state of ['later', 'now', 'done', '']) {
-    await page.keyboard.press(modKey + '+Enter', { delay: 10 })
+    await page.keyboard.press(modKey + '+Shift+Enter', { delay: 10 })
     await expect(page.locator('.block-content span >> nth=0')).toHaveClass(
       `inline ${state}`.trim()
     )
+    await expect(page.locator('.block-content span >> nth=1')).toHaveClass(
+      `inline ${state}`.trim()
+    )
   }
+})
+
+test('cycling TODO state while editing', async ({ page, block }) => {
+  await createRandomPage(page)
 
   await block.activeEditing(0)
   await block.mustFill('foobar')
   await expect(page.locator('textarea >> nth=0')).toHaveText('foobar')
   await expect(block.isEditing()).toBeTruthy()
   for (const state of ['LATER', 'NOW', 'DONE', '']) {
-    await page.keyboard.press(modKey + '+Enter', { delay: 10 })
+    await page.keyboard.press(modKey + '+Shift+Enter', { delay: 10 })
     await expect(page.locator('textarea >> nth=0')).toHaveText(
       `${state} foobar`
     )
@@ -40,26 +55,12 @@ test('cycling TODO state (SCHEDULED)', async ({ page, block }) => {
     { state: 'now', scheduled: '<2073-01-01 Sun .+73y>' },
   ]
   for (const expected of stateSeq) {
-    await page.keyboard.press(modKey + '+Enter', { delay: 10 })
+    await page.keyboard.press(modKey + '+Shift+Enter', { delay: 10 })
     await expect(page.locator('.block-content span >> nth=0')).toHaveClass(
       `inline ${expected.state}`
     )
     await expect(
       page.locator('.block-content .timestamp >> nth=0')
     ).toContainText(expected.scheduled)
-  }
-
-  await block.activeEditing(0)
-  await block.mustFill(`LATER foobar\nSCHEDULED: <2000-01-01 Sat .+73y>`)
-  stateSeq = [
-    { state: 'NOW', scheduled: '<2000-01-01 Sat .+73y>' },
-    { state: 'LATER', scheduled: '<2073-01-01 Sun .+73y>' },
-    { state: 'NOW', scheduled: '<2073-01-01 Sun .+73y>' },
-  ]
-  for (const expected of stateSeq) {
-    await page.keyboard.press(modKey + '+Enter', { delay: 10 })
-    await expect(page.locator('textarea >> nth=0')).toHaveText(
-      `${expected.state} foobar\nSCHEDULED: ${expected.scheduled}`
-    )
   }
 })
--- index.ts
+++ index.ts
@@ -1,7 +1,100 @@
 import '@logseq/libs'
+import type { BlockEntity } from '@logseq/libs/dist/LSPlugin'
+
+type TodoStyle = 'TODO' | 'LATER'
+
+async function preferredTodoStyle(): Promise<TodoStyle> {
+  const userConfigs = await logseq.App.getUserConfigs()
+  return userConfigs.preferredTodo as TodoStyle
+}
+
+async function getChosenBlocks(): Promise<[BlockEntity[], boolean]> {
+  const selected = await logseq.Editor.getSelectedBlocks()
+  if (selected) return [selected, true]
+
+  const uuid = await logseq.Editor.checkEditing()
+  if (!uuid) return [[], false]
+
+  const editingBlock = (await logseq.Editor.getBlock(
+    uuid as string
+  )) as BlockEntity
+
+  // to get ahead of Logseq block content saving process
+  editingBlock.content = await logseq.Editor.getEditingBlockContent()
+
+  return [[editingBlock], false]
+}
+
+const todoSequences = {
+  TODO: ['', 'TODO', 'DOING', 'DONE'] as const,
+  LATER: ['', 'LATER', 'NOW', 'DONE'] as const,
+} as const
+
+type MarkerTODOStyle = (typeof todoSequences.TODO)[number]
+type MarkerLATERStyle = (typeof todoSequences.LATER)[number]
+type Marker = MarkerTODOStyle | MarkerLATERStyle
+
+function getMarker(block: BlockEntity) {
+  let currentMarker = ''
+  const allPossibleMarkers = Object.values(todoSequences)
+    .flat()
+    .filter(
+      (marker, index, self) => marker !== '' && self.indexOf(marker) === index
+    )
+  const matchData = block.content?.match(
+    new RegExp(`^(${allPossibleMarkers.join('|')})`)
+  )
+  if (matchData) {
+    currentMarker = matchData[1]
+  }
+  return currentMarker as Marker
+}
+
+function setMarker(block: BlockEntity, newMarker: Marker) {
+  const content = block.content || ''
+  const currentMarker = getMarker(block)
+  if (currentMarker === '') {
+    return `${newMarker} ${content}`
+  } else {
+    return content
+      .replace(new RegExp(`^\\s*${currentMarker}`), newMarker)
+      .trim()
+  }
+}
+
+async function computeNextMarker(currentMarker: Marker) {
+  const userPreferredStyle = await preferredTodoStyle()
+  const todoSeq = todoSequences[userPreferredStyle] as readonly Marker[]
+  return todoSeq[(todoSeq.indexOf(currentMarker) + 1) % todoSeq.length]
+}
+
+async function cycleTODOdwim(): Promise<unknown> {
+  let [blocks] = await getChosenBlocks()
+  if (blocks.length === 0) return
+
+  for (const b of blocks) {
+    const currentMarker = getMarker(b)
+    const nextMarker = await computeNextMarker(currentMarker)
+    b.content = setMarker(b, nextMarker)
+  }
+  return Promise.all(
+    blocks.map((b) => logseq.Editor.updateBlock(b.uuid, b.content || ''))
+  )
+}
 
 async function main() {
-  logseq.UI.showMsg('cycle-todo-dwim AAAA')
+  logseq.App.registerCommandPalette(
+    {
+      label: 'Cycle TODO (Do What I Mean)',
+      key: 'cycle-todo-dwim',
+      keybinding: {
+        mac: 'mod+shift+enter',
+        binding: 'ctrl+shift+enter',
+        mode: 'global',
+      },
+    },
+    cycleTODOdwim
+  )
 }
 
 // bootstrap
